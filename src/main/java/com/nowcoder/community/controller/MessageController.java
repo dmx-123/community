4e8624ebef155b136adfabb3d31004920f6fe4f5
dmx-123 <mingxind48@gmail.com>
2024-12-18 03:55:34 -0500

    解决了6.2章节前的一些小bug

--- src/main/java/com/nowcoder/community/controller/MessageController.java
+++ src/main/java/com/nowcoder/community/controller/MessageController.java
@@ -168,8 +168,8 @@ public class MessageController implements CommunityConstant {
 
             int unread = messageService.findLetterUnreadCount(user.getId(), TOPIC_COMMENT);
             messageVO.put("unread", unread);
+            model.addAttribute("commentNotice", messageVO);
         }
-        model.addAttribute("commentNotice", messageVO);
 
         // 查询点赞类通知
         message = messageService.findLatestNotice(user.getId(), TOPIC_LIKE);
@@ -190,8 +190,8 @@ public class MessageController implements CommunityConstant {
 
             int unread = messageService.findLetterUnreadCount(user.getId(), TOPIC_LIKE);
             messageVO.put("unread", unread);
+            model.addAttribute("likeNotice", messageVO);
         }
-        model.addAttribute("likeNotice", messageVO);
 
         // 查询关注类通知
         message = messageService.findLatestNotice(user.getId(), TOPIC_FOLLOW);
@@ -211,8 +211,8 @@ public class MessageController implements CommunityConstant {
 
             int unread = messageService.findLetterUnreadCount(user.getId(), TOPIC_FOLLOW);
             messageVO.put("unread", unread);
+            model.addAttribute("followNotice", messageVO);
         }
-        model.addAttribute("followNotice", messageVO);
 
         // 查询未读消息数量
         int letterUnreadCount = messageService.findLetterUnreadCount(user.getId(), null);
